0f75826a044c39b42631ed86a6d34850a607705d
Lilac <lyliowy@gmail.com>
2019-07-27 17:21:44 +0100

    Added GUI

---
+++ src/main/java/com/songoda/ultimateclaims/utils/gui/OnClose.java
@@ -0,0 +1,11 @@
+package com.songoda.ultimateclaims.utils.gui;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public interface OnClose {
+
+    void OnClose(Player player, Inventory inventory);
+
+}
+
