cb50852d2ddf31e44c8c68751d6a4b13ccf1ae53
Brianna <bri@songoda.com>
2019-08-21 14:23:38 -0400

    up to date

--- src/main/java/com/songoda/ultimateclaims/utils/settings/Category.java
+++ src/main/java/com/songoda/ultimateclaims/utils/settings/Category.java
@@ -5,7 +5,7 @@ public enum Category {
     MAIN("General settings and options."),
 
     INTERFACES("These settings allow you to alter the way interfaces look.",
-            "They are used in GUI's to make paterns, change them up then open up a",
+            "They are used in GUI's to make patterns, change them up then open up a",
             "GUI to see how it works."),
 
     MYSQL("Settings regarding the Database."),
