2d80738f60df6312b87f0950f2b7c01261044cbb
Fernando Pettinelli <fernandopettinelli@l3tplay.com>
2021-03-30 14:52:36 -0300

    Refund money to owner on power cell destruction.

--- src/main/java/com/songoda/ultimateclaims/claim/PowerCell.java
+++ src/main/java/com/songoda/ultimateclaims/claim/PowerCell.java
@@ -1,6 +1,7 @@
 package com.songoda.ultimateclaims.claim;
 
 import com.songoda.core.compatibility.CompatibleMaterial;
+import com.songoda.core.hooks.EconomyManager;
 import com.songoda.core.hooks.HologramManager;
 import com.songoda.core.utils.TimeUtils;
 import com.songoda.ultimateclaims.UltimateClaims;
@@ -385,6 +386,7 @@ public class PowerCell {
             getItems().stream().filter(item -> item != null)
                     .forEach(item -> location.getWorld().dropItemNaturally(location, item));
             removeHologram();
+            EconomyManager.deposit(claim.getOwner().getPlayer(), economyBalance);
         }
         this.items.clear();
         if (opened != null)
